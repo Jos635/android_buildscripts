a0c46063b831008f620a2ba0aa72e5dcd6086ae9
Jos635 <jos635@outlook.com>
2017-12-12 15:37:12 +0100

    Added auto-detection for core count

--- make-build.sh
+++ make-build.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+NUM_CORES=grep -c ^processor /proc/cpuinfo
+
+echo "Using $NUM_CORES cores"
+
 source build/envsetup.sh
 
 lunch lineage_berlin-userdebug
 
-make -j 4
+make -j $NUM_CORES
 
 mka target-files-package dist
 
